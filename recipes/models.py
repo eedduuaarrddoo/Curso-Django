067c12f3641466c0a1b073372112697b834e10a5
Eduardo Afonso <edu_ardu@hotmail.com.br>
2024-01-30 21:33:32 -0300

    category/django admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -14,4 +21,9 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
