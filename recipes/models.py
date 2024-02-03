87e4e67416eea9c4a7dd703be444fa0e60d398af
Eduardo Afonso <edu_ardu@hotmail.com.br>
2024-02-02 22:25:00 -0300

    page category/usando dados do banco na home page

--- recipes/models.py
+++ recipes/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
-
-def __str__(self):
+    
+    def __str__(self):
         return self.name
 
 class Recipe(models.Model):
@@ -20,10 +20,14 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+    
+    def __str__(self):
+        return self.title
