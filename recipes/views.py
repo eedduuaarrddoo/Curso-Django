5b8e770148606f48a92e136a8ee7bca3bb3a4f1d
Eduardo Afonso <edu_ardu@hotmail.com.br>
2024-02-06 14:18:16 -0300

    recipe fix/views

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
-
+from django.shortcuts import render, get_object_or_404
+#from utils.recipes.factory import make_recipe
+from django.http import HttpResponse
 from .models import Recipe
+
 
 
 def home(request):
@@ -18,14 +19,29 @@ def category(request, category_id):
         category__id=category_id,
          is_published=True,
     ).order_by('-id')
+    
+    if not recipes:
+        return HttpResponse ('NOT FOUND', status=404)
+    
+    
     return render(request, 'recipes/pages/category.html', context={
-      
         'recipes': recipes,
+        'title': f'{recipes.first().category.name}- Category |'
+        
     })
 
 
 def recipe(request, id):
+   
+    """ recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first() 
+    """
+    
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+    
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
         'is_detail_page': True,
+        'recipe': recipe,
     })
