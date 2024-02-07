e43e6cd3a8e400e9b9be391bdc725211bee587ff
Eduardo Afonso <edu_ardu@hotmail.com.br>
2024-02-07 18:35:13 -0300

    iniciando testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        assert 1 == 1, '1'
+
+class RecipeURLsTest2(TestCase):
+    def test_the_pytest_is_ok(self):
+        assert 1 == 2, '1'
