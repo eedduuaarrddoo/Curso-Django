bf28a5ea0ed21805304275f2b34755dbeda3e9b6
Eduardo Afonso <edu_ardu@hotmail.com.br>
2024-03-20 12:08:30 -0300

    pagination/implementation

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,33 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Recipe
 from django.http.response import Http404
 from django.db.models import Q
+from utils.pagination import make_pagination_range
+
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page=1
+    
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range' : pagination_range
     })
 
 
